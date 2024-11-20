2ce0a6ea7514fa981e9f1b54b44a1735e4be9740
Hanson <chs415009@gmail.com>
2024-11-20 00:17:49 -0500

    generate test main

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,27 +10,27 @@ public class Main extends Application {
 
 	
     @Override
-//    public void start(Stage primaryStage) {
-//        try {
-//            FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/User/UserUI/Login.fxml"));
-//            Parent root = loader.load();
-//
-//            Scene scene = new Scene(root, 400, 300);
-//            primaryStage.setTitle("To Do List - Login");
-//            primaryStage.setScene(scene);
-//            primaryStage.show();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-    
-    // Monthly page test main code
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("monthly/ui/Monthly.fxml"));
-        primaryStage.setScene(new Scene(loader.load()));
-        primaryStage.setTitle("Monthly Calendar");
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/User/UserUI/Login.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root, 400, 300);
+            primaryStage.setTitle("To Do List - Login");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+    
+//    // Monthly page test main code
+//    public void start(Stage primaryStage) throws Exception {
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("monthly/ui/Monthly.fxml"));
+//        primaryStage.setScene(new Scene(loader.load()));
+//        primaryStage.setTitle("Monthly Calendar");
+//        primaryStage.show();
+//    }
 
     public static void main(String[] args) {
         launch(args);
